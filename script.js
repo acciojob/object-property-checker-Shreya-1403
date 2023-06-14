60990a6cddcd70c0e7ff02958c89ecdbfdc03d31
Shreya-1403 <67495439+Shreya-1403@users.noreply.github.com>
2023-06-14 21:51:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	var ans ;
+	for (let index = 0; index < sampleObject.length; index++) {
+		if (((sampleObject.red)==key)||(sampleObject.green)==key)||(sampleObject.white)==key) {
+			ans=true;
+		}
+	}
 }
 
 // Do not change the code below
