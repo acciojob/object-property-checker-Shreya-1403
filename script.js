b3a908eea78781fde2e19f92e306bcebd1de2cc3
Shreya-1403 <67495439+Shreya-1403@users.noreply.github.com>
2023-06-15 11:14:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 function hasKey(key) {
   //   write your code here
 		for(let x in sampleObject){
-			if (sampleObject["x"]===key) {
+			if (x===key) {
 				return true
 			}
 		}
